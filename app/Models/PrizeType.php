2c0da86be6dddb246632fcafb15cb95ff8053305
Dmitrij <plexb@yandex.ru>
2018-11-18 03:16:48 +0400

    Limited prize value for type

--- app/Models/PrizeType.php
+++ app/Models/PrizeType.php
@@ -14,4 +14,26 @@ class PrizeType extends Model
     const MONEY = 1;
     const BONUS = 2;
     const THING = 3;
+
+    const THINGS = [
+        'Телефон',
+        'Часы',
+        'Утюг',
+        'Камера',
+    ];
+
+    public static function getRandom()
+    {
+        return self::inRandomOrder()->first();
+    }
+
+    public function checkMoneyLimit(int $value)
+    {
+        return ($this->limit - $value) >= 0;
+    }
+
+    public function checkThingLimit()
+    {
+        return $this->limit > 0;
+    }
 }
---
+++ app/Services/Prize/Limiter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services\Prize;
+
+use App\Models\PrizeType;
+
+class Limiter
+{
+    public function handle(PrizeType $prizeType, $value)
+    {
+        switch ($prizeType->id) {
+            case PrizeType::MONEY:
+                $isAllow = $prizeType->checkMoneyLimit($value);
+                break;
+
+            case PrizeType::BONUS:
+                $isAllow = true;
+                break;
+
+            case PrizeType::THING:
+                $isAllow = $prizeType->checkThingLimit($value);
+                break;
+
+            default:
+                abort(404, 'Not found case for prize type.');
+                break;
+        }
+
+        return $isAllow;
+    }
+}
